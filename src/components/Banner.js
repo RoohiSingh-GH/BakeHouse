d05b7e1146699c0dfda0c91f0be2dfb6861650c2
Roohi Singh <singhroohi184@gmail.com>
2020-07-18 16:58:44 -0400

    updated
    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,13 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="container">
-      <header></header>
+      <header style={{ marginTop: "70px" }}>
+        <div className="headerText">
+          <div>
+            <p>Enjoy online shopping</p>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
