0ae95366d2e05607f72a0f7ab6ffc736dbc3827f
Roohi Singh <singhroohi184@gmail.com>
2020-07-18 16:02:50 -0400

    updated
    updated

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="left">
+        <li>
+          <a href="">Bakehouse</a>
+        </li>
+        <li></li>
+      </ul>
+      <ul className="right">
+        <li>
+          <a href="">
+            <span className="shoppingCart">
+              <i class="fas fa-cart-arrow-down"></i>
+              <span className="cartCount">0</span>
+            </span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
