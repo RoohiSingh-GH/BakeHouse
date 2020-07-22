acdcafa6ead0a55cd15d292982a667fc8b2b28d9
AVINASH NACHAGONI <66624018+avinashnachagoni@users.noreply.github.com>
2020-07-21 23:41:22 -0400

    Add files via upload

---
+++ src/quizData.js
@@ -0,0 +1,44 @@
+export const quizData = [
+    {
+        id: 0,
+        question: ` Which 'F' is a flat savoury Italian bread? `,
+        options: [`Fiber`, `Flat`, `Focaccia`],
+        answer: `Focaccia`
+    },
+    {
+        id: 1,
+        question: `Enchiladas originated in which country?`,
+        options: [`Canada`, `Mexico`, `Italy`],
+        answer: `Mexico`
+    },
+    {
+        id: 2,
+        question: `Bubble and Squeak is better known as meat, cabbage and which vegetable?`,
+        options: [`Potato`, `Carrot`, `Tomato`],
+        answer: `Potato`
+    },
+    {
+        id: 3,
+        question: `Blinis are stuffed pancakes best associated with which country?`,
+        options: [`Europe`, `India`, `Russia`],
+        answer: `Russia`
+    },
+    {
+        id: 4,
+        question: `In French cooking, what does 'en Croute' mean?`,
+        options: [`Chocolate`, `In Pastry`, `Cakes`],
+        answer: `In Pastry`
+    },
+    {
+        id: 5,
+        question: `Which 'C' is a thick fish or seafood soup?`,
+        options: [`Chowder`, `Cioppino`, `cacciucco`],
+        answer: `Chowder`
+    },
+    {
+        id: 6,
+        question: `Which 'P' is a quill-shaped Italian pasta?`,
+        options: [`Pappardelle`, `Penne`, `pen`],
+        answer: `Penne`
+    }
+];
